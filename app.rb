0218de03dc0232d01a9ddec7f8b3ec846b3715a3
jotase <jota.segovia@gmail.com>
2016-05-26 11:28:36 -0400

    init

---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'json'
+
+post '/caracola' do
+  if !params[:text].nil? || params[:text].strip[-1, 1] == '?'
+    response
+  else
+    respond_message 'Eso no es una pregunta'
+  end
+end
+
+def response
+  options = 'Algún día', 'Nada', 'Ni', 'Yo no lo creo', 'No', 'Si', 'Prueba preguntar otra vez'
+  respond_message options.sample
+end
+
+def respond_message(message)
+  content_type :json
+  { response_type: 'in_channel',
+    text: message,
+    username: '5Rabbit Bot',
+    mrkdwn: true }.to_json
+end
